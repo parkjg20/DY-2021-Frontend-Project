7452f00a586e3c186765c683366505de8b38dd42
jgpark-dataflow <jgpark@data-flow.co.kr>
2021-06-17 16:43:59 +0900

    본문보기 작업중

--- main.js
+++ main.js
@@ -4,39 +4,48 @@ var myInfo = {};
 var mainContent = {
     props: ["menu"],
     template: `
-    <div class="main-content card">
-        <h2>{{menu.title}}</h2>
-        <div class="heading">
-            <h4 class="people">{{ menu.info.people }}인분</h4>
-            <span class="divider"></span>
-            <h4 class="time">{{ menu.info.time }}분 이내</h4>
-            <span class="divider"></span>
-            <h4 class="difficulty low">난이도 {{ convertDifficultyToString(menu.info.difficulty) }}</h4>
-        </div>
-        <div class="content">
-            <div class="fakeimg" v-bind:style="{ background: 'url(' + menu.img + ') center center no-repeat', backgroundSize: 'cover' }"></div>
-            <div class="grocery">
-                <h2 class="grocery-header">[재료]</h2>
-                <div class="grocery-content">
-                    <ul class="main" v-for="grocery in menu.grocery">
-                        <li v-bind:class="{ 'title': index == 0 }" v-for="(item, index) in grocery">{{ item }}</li>
-                    </ul>
+       
+        <div v-bind:class="{'main-content': 1, 'card': 1, 'active': active}" v-if="">
+        <hr />
+            <h2>{{ menu.title }}</h2>
+            <div class="heading">
+                <h4 class="people">{{ menu.info.people }}인분</h4>
+                <span class="divider"></span>
+                <h4 class="time">{{ menu.info.time }}분 이내</h4>
+                <span class="divider"></span>
+                <h4 class="difficulty low">난이도 {{ convertDifficultyToString(menu.info.difficulty) }}</h4>
+            </div>
+            <div class="content">
+                <div class="fakeimg" v-bind:style="{ background: 'url(' + menu.img + ') center center no-repeat', backgroundSize: 'cover' }"></div>
+                <div class="grocery">
+                    <h2 class="grocery-header">[재료]</h2>
+                    <div class="grocery-content">
+                        <ul class="main" v-for="grocery in menu.grocery">
+                            <li v-bind:class="{ 'title': index == 0 }" v-for="(item, index) in grocery">{{ item }}</li>
+                        </ul>
+                    </div>
                 </div>
+                
+            <p v-html="menu.introduce"></p>
             </div>
-            
-        <p v-html="menu.introduce"></p>
+            <a @click="readFull()" class="more">
+                <div>
+                    ... 본문 보기
+                </div>
+            </a>
         </div>
-        <a href="#" class="more">
-            <div>
-                ... 본문 보기
-            </div>
-        </a>
-    </div>
     `,
     methods: {
-        convertDifficultyToString
+        convertDifficultyToString,
+        readFull: function() {
+            console.log(this.menu)
+            this.active = true;
+            this.$root.full = true;
+        }
     }, data: function() {
-        return {}
+        return {
+            active: (this.active == undefined)? false : this.active
+        }
     }
     
 }
@@ -47,38 +56,21 @@ var app = new Vue({
         'main-content': mainContent
     },
     data: {
-        fileIn: false,
-        menus,
-        myInfo
+        menus: [],
+        myInfo,
+        full: false
     }, methods: {
-        
+        readFull: function() {
+            full = true
+        }
     }
 })
 
 initDatas();
 
 function initDatas() {
+    jsonData();
     
-    readTextFile("file://\/menus.json")
-}
-
-function readTextFile(file)
-{
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-                alert(allText);
-            }
-        }
-    }
-    console.log(rawFile);
-    rawFile.send(null);
 }
 
 function convertDifficultyToString(difficulty) {
@@ -90,4 +82,23 @@ function convertDifficultyToString(difficulty) {
         case 2:
             return '상';
     }
+}
+
+function jsonData() {
+    var request = new XMLHttpRequest(0);
+    request.addEventListener('load', function(data) {
+        console.log("load", data);
+    })
+
+    request.addEventListener('readystatechange', function(xhr) {
+        if(xhr.currentTarget.readyState == 4) {
+            var json = JSON.parse(xhr.target.response);
+            console.log(json)
+            app.menus = json.data;
+            app.myInfo = json.myInfo;
+            console.log(app.menus, app.myInfo)
+        }
+    })
+    request.open("GET", "/menus.json");
+    request.send();
 }
